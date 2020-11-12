f819cf357f75e94912cfac9a0484f0835865bc85
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2020-11-12 15:36:36 -0800

    init

---
+++ src/com/tisuela/Item.java
@@ -0,0 +1,11 @@
+package com.tisuela;
+
+public class Item {
+
+    private double price;
+    private String name;
+
+    private double taxRate = 0.1;
+
+
+}
---
+++ src/com/tisuela/Main.java
@@ -0,0 +1,13 @@
+package com.tisuela;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x = 1;
+        Item testItem = new Item();
+        System.out.println("done!");
+
+
+    }
+}
